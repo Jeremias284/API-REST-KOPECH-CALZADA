0522efa0cb1537e4e883d18a891ea9f0adc873a8
ivan kopech <ivankopech@gmail.com>
2021-10-13 11:52:12 -0300

    API REST: metodo GET_BY_ID. Chequeado con Postman y MongoDB

--- src/controllers/patients/index.js
+++ src/controllers/patients/index.js
@@ -58,8 +58,34 @@ const deletePatientById = async (req, res) => {
     }
 };
 
+const getPatientById = async (req, res) => {
+    try {
+        const response = await PatientSchema.findOne({ _id: req.params.patientId})
+
+        if(!response || response.length === 0) {
+            return res.status(404).json({
+                error: true,
+                message: 'patient no found'
+            })
+        }
+
+        return res.status(200).json({
+            data: response,
+            error: false
+        })
+
+    } catch (error) {
+        return res.status(400).json({
+            error: true,
+            message: error
+        });
+        
+    }
+};
+
 module.exports = {
     getPatients,
+    getPatientById,
     addPatient,
     deletePatientById
 }
--- src/routes/patients/index.js
+++ src/routes/patients/index.js
@@ -4,6 +4,7 @@ const controller = require('../../controllers/patients');
 const router = express.Router();
 
 router.route('/').get(controller.getPatients);
+router.route('/:patientId').get(controller.getPatientById);
 router.route('/').post(controller.addPatient);
 router.route('/:patientId').delete(controller.deletePatientById);
 
