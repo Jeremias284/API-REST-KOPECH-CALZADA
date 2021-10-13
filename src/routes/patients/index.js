d4ec7b72b2738452de69215c47b38be9dd0330f1
ivan kopech <ivankopech@gmail.com>
2021-10-13 11:54:07 -0300

    API REST: comentarios sobre la funcion de cada metodo

--- src/routes/patients/index.js
+++ src/routes/patients/index.js
@@ -3,9 +3,13 @@ const controller = require('../../controllers/patients');
 
 const router = express.Router();
 
+//OBTIENE TODOS LOS ELEMENTOS DE LA COLECCION
 router.route('/').get(controller.getPatients);
+//OBTIENE EL ELEMENTO CORRESPONDIENTE AL ID INDICADO EN LA REQUEST
 router.route('/:patientId').get(controller.getPatientById);
+//AGREGA UN ELEMENTO A LA COLECCION
 router.route('/').post(controller.addPatient);
+//ELIMINA UN ELEMENTO DE LA COLECCION INDICANDO EL ID DEL MISMO
 router.route('/:patientId').delete(controller.deletePatientById);
 
 module.exports = router;
